00b8be8c10870cfce3dff67d3ec9afe3cea37100
Julien Schueller <schueller@phimeca.com>
2025-09-05 18:14:21 +0200

    Fix omniorb dl

--- docker/manylinux/build_omniorb.sh
+++ docker/manylinux/build_omniorb.sh
@@ -11,7 +11,7 @@ OMNIORB_VERSION=4.2.5
 PATH=/opt/python/${PYTAG}-${ABI}/bin/:$PATH
 
 cd /tmp
-curl --retry 10 -LO https://downloads.sourceforge.net/omniorb/omniORB-${OMNIORB_VERSION}.tar.bz2
+# curl --retry 10 -LO https://downloads.sourceforge.net/omniorb/omniORB-${OMNIORB_VERSION}.tar.bz2
 tar xfj omniORB-${OMNIORB_VERSION}.tar.bz2
 cd omniORB*
 ./configure --with-openssl=/usr
@@ -19,7 +19,7 @@ make > /dev/null 2>&1
 make install > /dev/null 2>&1
 
 cd /tmp
-curl --retry 10 -LO https://downloads.sourceforge.net/omniorb/omniORBpy/omniORBpy-${OMNIORB_VERSION}/omniORBpy-${OMNIORB_VERSION}.tar.bz2
+# curl --retry 10 -LO https://downloads.sourceforge.net/omniorb/omniORBpy/omniORBpy-${OMNIORB_VERSION}/omniORBpy-${OMNIORB_VERSION}.tar.bz2
 tar xfj omniORBpy-${OMNIORB_VERSION}.tar.bz2
 cd omniORBpy*
 patch -p1 -i /tmp/omniorb-noinitfile.patch
@@ -29,4 +29,4 @@ make install > /dev/null 2>&1
 
 PYTHONPATH=/usr/local/lib/python${PYVERD}/site-packages python -c "from omniORB import CORBA, PortableServer"
 
-rm -r /tmp/omniORB*
+rm -r /tmp/omniORB-${OMNIORB_VERSION} /tmp/omniORBpy-${OMNIORB_VERSION}
